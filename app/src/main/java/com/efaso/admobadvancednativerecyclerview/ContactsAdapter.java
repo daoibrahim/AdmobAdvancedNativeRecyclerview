a5a034d0db12fe60ae39f193ebb878363672984c
braxmarelax <daoibrahim2@gmail.com>
2020-01-07 11:18:02 +0000

    Sample app made

---
+++ app/src/main/java/com/efaso/admobadvancednativerecyclerview/ContactsAdapter.java
@@ -0,0 +1,75 @@
+package com.efaso.admobadvancednativerecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactsAdapter extends
+        RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public Button messageButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+    private List<Contact> mContacts;
+
+    // Pass in the contact array into the constructor
+    public ContactsAdapter(List<Contact> contacts) {
+        mContacts = contacts;
+    }
+
+    @Override
+    public ContactsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(ContactsAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Contact contact = mContacts.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = viewHolder.messageButton;
+        button.setText(contact.isOnline() ? "Message" : "Offline");
+        button.setEnabled(contact.isOnline());
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+}
--- app/src/main/java/com/efaso/admobadvancednativerecyclerview/MainActivity.java
+++ app/src/main/java/com/efaso/admobadvancednativerecyclerview/MainActivity.java
@@ -1,14 +1,52 @@
 package com.efaso.admobadvancednativerecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.efaso.admob_advanced_native_recyvlerview.AdmobNativeAdAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Contact> contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List <String> list=new ArrayList<>();
+        for(int i=0;i<30;i++){
+            list.add("Let's save the world "+i);
+        }
+
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+
+        //Build the native adapter from the current adapter
+        AdmobNativeAdAdapter admobNativeAdAdapter=AdmobNativeAdAdapter.Builder.with(
+                "ca-app-pub-3940256099942544/2247696110",//admob native ad id
+                adapter,//current adapter
+                "small"//Set the size "small", "medium" or "custom"
+                ).adItemInterval(5)//Repeat interval
+                .build();
+
+
+        // Attach the new adapter to the recyclerview to populate items
+        rvContacts.setAdapter(admobNativeAdAdapter);
+
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
